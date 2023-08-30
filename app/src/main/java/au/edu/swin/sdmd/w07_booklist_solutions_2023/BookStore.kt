8f4b19d61cfecec3b6320aa48350a66a04c2b9e3
N. A. Ronald <nronald@swin.edu.au>
2023-08-30 15:36:54 +1000

    Add BookStore object.

---
+++ app/src/main/java/au/edu/swin/sdmd/w07_booklist_solutions_2023/BookStore.kt
@@ -0,0 +1,13 @@
+package au.edu.swin.sdmd.w07_booklist_solutions_2023
+
+object BookStore {
+
+    val books = listOf(
+        Book("Mobilities", 4.0F, "2581744"),
+        Book("Pascal", 1.0F, "9999512"),
+        Book("The Hitchhiker's Guide to the Galaxy", 5.0F, "8594912"),
+        Book("Concepts of modern art", 2.0F,"316050")
+    )
+
+    val count = books.size
+}
