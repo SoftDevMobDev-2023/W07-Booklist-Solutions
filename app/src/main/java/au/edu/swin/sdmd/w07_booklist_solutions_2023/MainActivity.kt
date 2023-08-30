8214cca865166b2dd97054fdd4db5ce481a480b4
N. A. Ronald <nronald@swin.edu.au>
2023-08-30 15:47:23 +1000

    Finalise MainActivity with list.

--- app/src/main/java/au/edu/swin/sdmd/w07_booklist_solutions_2023/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w07_booklist_solutions_2023/MainActivity.kt
@@ -2,10 +2,17 @@ package au.edu.swin.sdmd.w07_booklist_solutions_2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = findViewById<RecyclerView>(R.id.booklist)
+
+        list.adapter = BooklistAdapter()
+        list.layoutManager = LinearLayoutManager(this)
     }
 }
